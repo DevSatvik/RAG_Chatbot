63e5558d9ca1645fb04a6ed65802a07252f55c83
Satvik <satvik2saxena@gmail.com>
2025-01-28 12:46:23 +1100

    logic css

--- nextjs-rag-gpt/app/page.tsx
+++ nextjs-rag-gpt/app/page.tsx
@@ -14,12 +14,12 @@ const Home = () => {
     const noMessages = true;  
 
     return (
-    <section className='bg-[#E8E8E8] flex flex-col items-center justify-between h-4/6 w-fit rounded-lg  '>
+    <section className='bg-[#E8E8E8] flex flex-col items-center justify-between h-4/6 w-fit rounded-lg p-6 shadow-lg'>
         <Image src={crickgptLogo} width="250" alt='CrickGPT Logo' priority className=''/>
         <section className={noMessages ? '' : ''}>
             {noMessages ? (
                 <>
-                    <p className='justify-center items-center flex text-center text-gray-700 border-x-8'>
+                    <p className='text-center text-gray-700 px-8 py-2'>
                         Ask CrickGPT anything about cricket! <br /> 
                         It will come back with the most up to date answers. Enjoy!
                     </p>
@@ -33,17 +33,21 @@ const Home = () => {
                 </>
             )}
         </section>
+        
         <form onSubmit={handleSubmit}
-                className='h-10 w-full flex justify-evenly items-center'
+                className='h-10 w-full '
         >
+                <hr className="w-full border-t border-gray-300" />
                 <input 
-                    className='w-full mx-5 rounded-lg text-left pl-2 my-2'
+                    className='w-5/6 px-4 py-3 text-base border-none shadow focus:outline-none focus:ring-2 focus:ring-blue-300 '
                     onChange={handleInputChange}
                     value={input}
                     placeholder="Ask CrickGPT anything about cricket!"
                 />
-                <input className='' type="submit" />
-            </form>
+                <input className='w-1/6 px-4 py-2  bg-blue-500 hover:bg-blue-700 text-white text-center font-bold rounded cursor-pointer transition-colors duration-200 ease-in-out' 
+                        type="submit" 
+                        value="Submit" />
+        </form>
     </section>
   )
 }
