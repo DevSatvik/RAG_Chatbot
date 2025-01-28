92cd47d2263d532efcf92139aa88cb8ab5953183
Satvik <satvik2saxena@gmail.com>
2025-01-28 17:37:01 +1100

    css changes

--- nextjs-rag-gpt/app/components/Bubble.tsx
+++ nextjs-rag-gpt/app/components/Bubble.tsx
@@ -4,11 +4,18 @@ const Bubble = ({ message }) => {
     const {content, role} = message;
 
     return (
-    <div className='m-2 p-2 text-base border-none bg-[#383838] shadow-lg w-4/5 text-left'>
-        <div className= {role === 'user' ? 'bg-[E1F4FFFF] ml-auto rounded-tl-2xl rounded-tr-2xl rounded-br-none rounded-bl-2xl' : 'bg-[#dce7ff] text-black rounded-tl-2xl rounded-tr-2xl rounded-br-2xl rounded-bl-none'}>
-            {content}
-        </div>
+    // <div className='m-2 p-2 text-base border-none   w-4/5 text-left'>
+    //     <div className= {role === 'user' ? 'border- bg-[E1F4FFFF] ml-auto rounded-tl-2xl rounded-tr-2xl rounded-br-none rounded-bl-2xl' : 'bg-[#dce7ff] text-black rounded-tl-2xl rounded-tr-2xl rounded-br-2xl rounded-bl-none'}>
+    //         {content}
+    //     </div>
+    // </div>
+    <div className='m-2 p-2 text-base border-none w-4/5 '>
+    <div className={role === 'user' ? 
+        'border border-blue-300 bg-[#E1F4FF] mr-auto rounded-tl-2xl rounded-tr-2xl rounded-br-none rounded-bl-2xl ' : 
+        'border border-gray-300 bg-[#dce7ff] text-black ml-auto rounded-tl-2xl rounded-tr-2xl rounded-br-2xl rounded-bl-none'}>
+        {content}
     </div>
+</div>
   )
 }
 
